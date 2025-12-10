d0c1a4b763e9474b1ce27afe271f6e00aa6cbfbe
ekansh12-collab <ekanshsahay1205@gmail.com>
2025-12-10 16:44:41 +0530

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,11 @@
+a=int(input('enter your birth day'))
+if a==12:
+    print('its ekansh birthday')
+elif a==24:
+    print('its mangal birthday')
+elif a==21:
+    print('its dipti birthday')
+elif a==11:
+    print('its gudia birthday')
+else:
+    print('its not your birthday')
